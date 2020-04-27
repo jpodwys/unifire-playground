24f82e68953715b6e898064d88677e662800b1cc
Joseph Podwys <joseph@podwys.com>
2020-04-26 23:41:08 -0600

    Initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
+import { Observer } from '../../js/unifire/preact';
+
+const Header = ({ one, two, count, fire }) => {
+	console.log("HEADER");
+	return (
+		<header class={style.header}>
+			<h1>Preact App Count: {count}</h1>
+			<nav>
+				<button onClick={() => fire('decrement')}>Decrement</button>
+				<button onClick={() => fire('increment')}>Increment</button>
+				<Link activeClassName={style.active} href="/">Home</Link>
+				<Link activeClassName={style.active} href="/profile">Me</Link>
+				<Link activeClassName={style.active} href="/profile/john">John</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default Observer(Header);
---
+++ src/js/store/index.js
@@ -0,0 +1,43 @@
+// import { Unifire } from '../unifire';
+import { Unifire } from '../unifire/batch';
+
+const wait = (delay = 1000) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
+const state = {
+  one: 1,
+  two: 2,
+  count: parseInt(localStorage.getItem('count'), 10) || 0,
+  list: [ 'one', 'two', 'three', 'four' ]
+};
+
+const actions = {
+  increment: ({ state }) => {
+    state.count++;
+  },
+  decrement: ({ state }) => state.count--,
+  changeBoth: ({ state }) => {
+    state.one++;
+    state.two++;
+  },
+  changeBothAsync: async ({ state }) => {
+    state.count++;
+    state.one++;
+    await Promise.resolve();
+    state.count++;
+  },
+  waitFiveSeconds: async ({ state }) => {
+    await wait();
+    state.count--;
+  }
+};
+
+export const store = Unifire({ state, actions });
+
+store.subscribe(({ count }, { prior }) => {
+  localStorage.setItem('count', count);
+  console.log('COUNT', count, prior)
+});
